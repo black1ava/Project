3ed03b051919ccd9770af4b6d5dd7a4680e76fd6
black1ava <73036266+black1ava@users.noreply.github.com>
2020-11-21 12:36:37 +0700

    Add files via upload

---
+++ Contact Manager/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Component/Header'
+import Body from './Component/Body'
+
+export default function App(){
+  return(
+    <div>
+      <Header />
+      <Body />
+    </div>
+  )
+}
