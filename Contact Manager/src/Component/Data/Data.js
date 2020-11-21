38cd5f94e1e61ab496d7858c284030ef238d3f21
black1ava <73036266+black1ava@users.noreply.github.com>
2020-11-21 12:41:35 +0700

    Create Data.js

---
+++ Contact Manager/src/Component/Data/Data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    id: 1,
+    name: 'Alpha',
+    edit: false
+  },
+  {
+    id: 2,
+    name: 'Beta',
+    edit: false
+  },
+  {
+    id: 3,
+    name: 'Gamma',
+    edit: false
+  }
+]
+
+export default data
