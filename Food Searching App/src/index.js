5278d28e3708173e74537979738a1dc5fe6568af
black1ava <73036266+black1ava@users.noreply.github.com>
2020-11-30 12:21:24 +0700

    Create index.js

---
+++ Food Searching App/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './App.css'
+import './index.css'
+import './Component/Display/Style/Display.css'
+import './Component/Display/Style/Alert.css'
+import './Component/Display/Style/Menu.css'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
