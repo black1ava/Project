e4e38ac5dbc879ad67c35f89bb2e9d98401dd261
black1ava <73036266+black1ava@users.noreply.github.com>
2020-11-30 12:23:39 +0700

    Create Header.js

---
+++ Food Searching App/src/Component/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header(){
+  return(
+    <div>
+      <center>
+        <h1>
+          <a 
+            className="header" 
+            href=""
+          >
+            Food Searching App
+          </a>
+        </h1>
+      </center>
+    </div>
+  )
+}
