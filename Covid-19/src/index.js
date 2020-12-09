f1dfe82f77ef7beb8f6af98959c83ae5674cc579
black1ava <73036266+black1ava@users.noreply.github.com>
2020-12-09 20:46:21 +0700

    Create index.js

---
+++ Covid-19/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+import './Component/Style/Header.css'
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
