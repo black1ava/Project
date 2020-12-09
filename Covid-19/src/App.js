b8e445bf70873823f33b99cf6f0fc9db7d11005b
black1ava <73036266+black1ava@users.noreply.github.com>
2020-12-09 20:47:08 +0700

    Add files via upload

---
+++ Covid-19/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Header from './Component/Header'
+import Body from './Component/Body'
+
+export default function App(){
+  return(
+    <div>
+      <Header />
+      <Body />
+    </div>
+  )
+}
